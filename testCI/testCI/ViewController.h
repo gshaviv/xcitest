b0fd6f7325598d98189ee5285a93bcd4df1ee547
Guy Shaviv <guy@tivstudio.com>
2015-12-13 13:28:22 +0200

    commit 1

---
+++ testCI/testCI/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testCI
+//
+//  Created by Guy on 13/12/2015.
+//  Copyright © 2015 Guy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
