35224dfec2f5b7be989d01d7f4b94bff118b492e
Guy Shaviv <guy@tivstudio.com>
2015-12-16 14:49:32 +0200

    6

--- testCI/AppDelegate.m
+++ testCI/AppDelegate.m
@@ -16,7 +16,15 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    __block NSInteger i = 0;
+    static dispatch_source_t timer;
     // Override point for customization after application launch.
+    timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, dispatch_get_main_queue());
+    dispatch_source_set_timer(timer, DISPATCH_TIME_NOW, 2 * NSEC_PER_SEC, 1000 * NSEC_PER_SEC);
+    dispatch_source_set_event_handler(timer, ^{
+        NSLog(@"Ping %d",i++);
+    });
+    dispatch_resume(timer);
     return YES;
 }
 
