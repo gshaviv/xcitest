30d181b861b775f1c9bc2552fc056107e424a603
Guy Shaviv <guy@tivstudio.com>
2015-12-16 13:28:14 +0200

    4

---
+++ testCI/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testCI
+//
+//  Created by Guy on 13/12/2015.
+//  Copyright © 2015 Guy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+- (BOOL) returnTrue;
+
+@end
+
